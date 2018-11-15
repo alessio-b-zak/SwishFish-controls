87910bf956aee1b7984651cc20d3f479ac65fb24
Alessio Zakaria <alessio.b.zakaria@gmail.com>
2018-11-15 12:07:09 +0000

    corrected rotations

--- Sweep.ino
+++ Sweep.ino
@@ -31,21 +31,26 @@ void setup() {
   Serial.println(" Type in Box above, . ");
   Serial.println("(Decimal)(Hex)(Character)");  
   Serial.println(); 
-  myservo0.write(90);
+  myservo1.write(90);
+  myservo2.write(0);
 
 }
 
 void rotate_servos() {
-    for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
-    // in steps of 1 degree
-    myservo1.write(pos);
+    int possec;
+    for (pos = 0; pos <= 90; pos += 1) { // goes from 0 degrees to 180 degrees
+    // in steps of  1 degree
+    possec = 90 - pos;
+    myservo1.write(possec);
     myservo2.write(pos);// tell servo to go to position in variable 'pos'
-    delay(1);                       // waits 15ms for the servo to reach the position
+    delay(2);                       // waits 15ms for the servo to reach the position
   }
-  for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
-    myservo1.write(pos);
+
+  for (pos = 90; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+    possec = (90-pos);
+    myservo1.write(possec);
     myservo2.write(pos);// tell servo to go to position in variable 'pos'
-    delay(1);                       // waits 15ms for the servo to reach the position
+    delay(2);                       // waits 15ms for the servo to reach the position
   }
 }
 
@@ -61,6 +66,9 @@ void loop() {
     Serial.print(char(ByteReceived));
     if(ByteReceived == '1') // Single Quote! This is a character.
     {
+      rotate_servos();
+      myservo1.write(90);
+      myservo2.write(0);
       Serial.print(" LED ON ");
     }
     if(ByteReceived == '2')
@@ -69,17 +77,16 @@ void loop() {
     }
     if(ByteReceived == '3')
     {
-      myservo0.write(180);
-    }
+    myservo1.write(90);
+      myservo2.write(0);
+      }
     if(ByteReceived == '4')
     {
       myservo0.write(90);
     }
     Serial.println();    // End the line
 
-
   // END Serial Available
   }
-  rotate_servos();
 
 }
